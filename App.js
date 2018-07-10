57584bd138bb7496b944def9d29e30f63be47bfb
Michael Andorfer <office@michael-andorfer.com>
2018-07-10 10:55:28 +0200

    implement microphone permission request

--- App.js
+++ App.js
@@ -9,8 +9,11 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Alert
 } from 'react-native';
+
+import Permissions from 'react-native-permissions';
 
 // import AudioRecorderUIView from './AudioRecorderNativeView'
 import AudioRecorderNative from './AudioRecorderNativeModule'
@@ -18,6 +21,7 @@ import AudioRecorderNative from './AudioRecorderNativeModule'
 type Props = {};
 export default class App extends Component<Props> {
   state = {
+    microphonePermission: null,
     recording: false
   };
 
@@ -33,17 +37,54 @@ export default class App extends Component<Props> {
     }
   };
 
+  requestPermission = () => {
+    Permissions.request("microphone").then(response => {
+      console.log(response);
+      // Returns once the user has chosen to 'allow' or to 'not allow' access
+      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+      this.setState({ microphonePermission: response });
+    });
+  };
+
+  showPermissionAlert = () => {
+    Alert.alert(
+      "Record audio",
+      "Audvise requires microphone permission to record audio",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Permission denied"),
+          style: "cancel"
+        },
+        this.state.microphonePermission == "undetermined"
+          ? { text: "OK", onPress: this.requestPermission }
+          : { text: "Open Settings", onPress: Permissions.openSettings }
+      ]
+    );
+  };
+
+  componentDidMount = async () => {
+    const microphonePermission = await Permissions.check("microphone");
+
+    this.setState({ microphonePermission }, () => {
+      if (microphonePermission === "undetermined") {
+        this.showPermissionAlert();
+      }
+    });
+  };
+
   render() {
-    const { recording } = this.state;
+    const { microphonePermission, recording } = this.state;
 
     return (
       <View style={styles.container}>
-        <Button
-          style={styles.button}
-          onPress={this.handlePress}
-          title={recording ? "Stop recording" : "Start recording"}
-        />
-
+        {microphonePermission === "authorized" && (
+          <Button
+            style={styles.button}
+            onPress={this.handlePress}
+            title={recording ? "Stop recording" : "Start recording"}
+          />
+        )}
       </View>
     );
   }

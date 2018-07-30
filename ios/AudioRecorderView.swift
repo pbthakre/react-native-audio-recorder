83580caefb8e50bacf5aa08effa0b4e426ada805
Michael Andorfer <office@michael-andorfer.com>
2018-07-30 11:42:03 +0200

    implement rendering of scrubber

--- ios/AudioRecorderView.swift
+++ ios/AudioRecorderView.swift
@@ -13,6 +13,7 @@ import AudioKitUI
 // Represents the our native ui (view) component
 class AudioRecorderView: EZAudioPlot {
   var plot : AKNodeOutputPlot = AKNodeOutputPlot(AKMixer.init(), frame: CGRect.init())
+  private var timelineBar = TimelineBar()
   
   private override init(frame: CGRect) {
     // Call super constructor
@@ -55,6 +56,10 @@ class AudioRecorderView: EZAudioPlot {
       
       // Clear the waveform to generate a baseline
       self.plot.clear()
+      
+      self.timelineBar.updateSize()
+      self.timelineBar.autoresizingMask = [.flexibleHeight]
+      self.plot.addSubview(self.timelineBar)
     
       // Add the view
       self.addSubview(self.plot)
@@ -68,5 +73,30 @@ class AudioRecorderView: EZAudioPlot {
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+class TimelineBar: AKView {
+  private let color = UIColor.white
+  private var rect = CGRect(x: 50, y: 0, width: 2, height: 70)
+  
+  convenience init() {
+    self.init(frame: CGRect(x: 50, y: 0, width: 2, height: 70))
+    // wantsLayer = true
+  }
+  
+  public func updateSize(height: CGFloat = 0) {
+    guard let superview = superview else { return }
+    let theHeight = height > 0 ? height : superview.frame.height
+    rect = CGRect(x: 0, y: 0, width: 2, height: superview.frame.height)
+  }
+  
+  override func draw(_ dirtyRect: CGRect) {
+    let context = UIGraphicsGetCurrentContext()
+    if (context != nil) {
+      context?.setShouldAntialias(false)
+    }
+    color.setFill()
+    context?.fill(dirtyRect)
   }
 }

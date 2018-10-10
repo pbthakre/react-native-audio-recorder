943480e8cc24ec0433f05adb4bcececf604e9218
Michael Andorfer <office@michael-andorfer.com>
2018-10-10 15:56:54 +0200

    chore: update audiokit from 4.5.0 to 4.5.1 which fixes an app crashing problem with microphone usage

--- ios/Frameworks/AudioKit.framework/Headers/AudioKit-Swift.h
+++ ios/Frameworks/AudioKit.framework/Headers/AudioKit-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31dab87bfb694cb687264b7e237ec163b8069ea35939f128e6486ac492ee5d20
-size 264856
+oid sha256:4705ee11cfec6264a1e3abb4bee70dac8fc3e3f987ff620334f5e08382020df5
+size 264954

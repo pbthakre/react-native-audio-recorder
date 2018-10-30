f94f12d6c912ea7c84d6ec36f004ebcf51e36a6a
Michael Andorfer <office@michael-andorfer.com>
2018-10-30 09:21:49 +0100

    android: refactor audio-player-view

--- android/src/main/java/com/reactlibrary/AudioPlayerPlot/AudioPlayerView.java
+++ android/src/main/java/com/reactlibrary/AudioPlayerPlot/AudioPlayerView.java
@@ -13,32 +13,53 @@ import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.widget.RelativeLayout;
-
+import com.reactlibrary.Helpers.FileUtils;
 import com.reactlibrary.R;
-
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
+// Represents the player plot waveform wrapper
 public class AudioPlayerView extends RelativeLayout {
-  private Context context;
-
   // The plot which represents the waveform
   private StaticWaveformView plot;
 
-  // Method to listen for update requests
+  // The constructor
+  public AudioPlayerView(Context context) {
+    super(context);
+
+    // Register the event bus
+    EventBus.getDefault().register(this);
+
+    // Apply layout from xml
+    inflate(context, R.layout.audio_player_view, this);
+
+    // Create the plot with the layout defined in the xml
+    this.plot = findViewById(R.id.audio_player_waveform);
+
+    // Set the plot line color
+    this.plot.setColor();
+
+    // Define the number of bars used for the waveform (1 - 256)
+    // More than 256 means that the the bars are overlapping and the waveform get its
+    // characteristic style
+    this.plot.setDensity(257f);
+
+    // Set the plot data with the data from the file
+    this.plot.setData(null);
+  }
+
+  // Method to listen for waveform update requests
   @Subscribe(threadMode = ThreadMode.MAIN)
   public void onWaveformEvent(WaveformEvent event) {
+    // Update the waveform
     if (event.code == 1) {
       this.updateWaveformWithData(event.fileUrl);
     }
 
+    // Set the properties received from RN
     if (event.code == 2) {
       if (event.backgroundColor != null) {
         this.plot.backgroundColor = Color.parseColor(event.backgroundColor);
@@ -52,76 +73,28 @@ public class AudioPlayerView extends RelativeLayout {
     }
   }
 
-  // The constructor
-  public AudioPlayerView(Context context) {
-    super(context);
-    this.context = context;
-    EventBus.getDefault().register(this);
-    this.init();
-  }
-
-  // Reads the bytes of a file
-  public static byte[] fileToBytes(File file) {
-    // Get the file size in bytes
-    int size = (int) file.length();
-
-    // Create the array where the bytes are stored in
-    byte[] bytes = new byte[size];
-
-    try {
-      // Read the bytes and store it in the array
-      BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
-      buf.read(bytes, 0, bytes.length);
-      buf.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return bytes;
-  }
-
-  // Initialize the waveform
-  public void init() {
-    // Apply layout from xml
-    inflate(context, R.layout.audio_player_view, this);
-
-    // Create the plot with the layout defined in the xml
-    this.plot = findViewById(R.id.audio_player_waveform);
-
-    // Set the plot line color
-    this.plot.setColor();
-
-    // Define the number of bars used for the waveform (1 - 256)
-    // More than 256 means that the the bars are overlapping and the waveform get its
-    // characteristic style
-    this.plot.setDensity(1024f);
-
-    // Set the plot data with the data from the file
-    this.plot.setData(null);
-  }
-
   // Add the data to the plot (waveform)
-  public void updateWaveformWithData(String fileUrl)  {
+  private void updateWaveformWithData(String fileUrl)  {
+    // Remove timestamp parameter
     String filePathCleaned = fileUrl.substring(0, fileUrl.indexOf("?"));
 
-    // Get a instance of the file
+    // Get an instance of the file
     File audioFile = new File(filePathCleaned);
 
     // Read the file duration from the file meta data
     Uri uri = Uri.parse(audioFile.getAbsolutePath());
     MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-    mmr.setDataSource(null,uri);
+    mmr.setDataSource(null, uri);
     String durationStr = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
     float durationInMS = Float.parseFloat(durationStr);
 
     // Set the duration
     this.plot.setFileDuration(durationInMS);
 
-    // Read the data
-    byte[] data = fileToBytes(audioFile);
+    // Read the file data
+    byte[] data = FileUtils.fileToBytes(audioFile);
 
-    // Set the plot data with the data from the file
+    // Set the plot data to be the data of the audio file
     this.plot.setData(data);
   }
 }
